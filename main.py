0743265b99bd482d8e1818b7cfbffa21dd0191f1
ralucaj <raluca.jalaboi@gmail.com>
2022-01-11 16:21:01 +0100

    Hello again gcp

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 
 
 if __name__ == '__main__':
-    print('Hello gcp')
+    print('Hello')
     digits = datasets.load_digits()
 
     # flatten the images
